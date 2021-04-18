7533b87602d5b314c4d24c5c727c03e23046c173
Keriteal <keriteal985@gmail.com>
2021-04-18 16:03:18 +0800

    Add ownable back

--- contracts/EtherplantFactory.sol
+++ contracts/EtherplantFactory.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 abstract contract IEtherplantFactory {
     enum Quality {N, R, SR, SSR}
