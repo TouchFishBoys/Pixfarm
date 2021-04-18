020705da273818688ce14fa15a3da5afce84fea8
Keriteal <keriteal985@gmail.com>
2021-04-18 19:26:04 +0800

    Update test

--- test/dna_test.js
+++ test/dna_test.js
@@ -2,9 +2,9 @@ const EtherplantFactory = artifacts.require("EtherplantFactory")
 
 contract("Plant factory test", async accounts => {
     it("should dna generated correctly", async () => {
-        let testedDna = 197346
-        let instance = await EtherplantFactory.deployed()
-        let properties = await instance.getPlantProperties(testedDna)
+        let testedDna = 197346;
+        let instance = await EtherplantFactory.deployed();
+        let properties = await instance.getPlantProperties(testedDna);
 
         let def = properties.def;
         let spd = properties.spd;
