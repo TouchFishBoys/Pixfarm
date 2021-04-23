5f5dfdf3b41442002419afab60349c4c952cd2b1
Keriteal <keriteal985@gmail.com>
2021-04-23 14:58:07 +0800

    Move PlantPropertiesPacked to FarmBase

--- contracts/FarmBase.sol
+++ contracts/FarmBase.sol
@@ -1,4 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "./RespositoryBase.sol"
-contract FarmBase {}
+contract FarmBase {
+    /// @dev 属性封包
+    struct PlantPropertiesPacked {
+        Specie specie;
+        uint8 hp;
+        uint8 atk;
+        uint8 def;
+        uint8 spd;
+    }
+}
