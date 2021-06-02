24d9d489e1d53a01ea5ee73bbdbbce11d37a8fb3
Keriteal <keriteal985@gmail.com>
2021-06-02 12:04:57 +0800

    Add Compiled Constracts

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
+      host: "54.144.133.190",
+      port: 8545,
       network_id: "*"
     }
   },
